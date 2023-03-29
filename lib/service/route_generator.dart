1b29de4eb9b24cece694b37bf1d4f01db43d7bd8
mah454 <mah454@gmail.com>
2023-03-29 11:52:45 +0330

    change dart file name

--- lib/service/route_generator.dart
+++ lib/service/route_generator.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:roboexchange_ui/pages/login_page.dart';
-import 'package:roboexchange_ui/pages/trend_line_list_page.dart';
+import 'package:roboexchange_ui/pages/trend_line_page.dart';
 
 class RouteHandler {
   static Route<dynamic> handle(RouteSettings settings) {
